fe0d4b64b07bb36858d381e2e603ec143fc239db
Anatoly Koshlyak <air721@yandex.ru>
2023-01-07 20:44:11 +0600

    Проект на проверку после ревью 1

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,19 @@
-let popupElement = document.querySelector('.popup');
-let popupCloseButtonElement = popupElement.querySelector('.popup__close');
-let popupOpenButtonElement = document.querySelector('.edit-button');
+let formElement = document.querySelector('.form');
+let formCloseButtonElement = formElement.querySelector('.form__close');
+let formOpenButtonElement = document.querySelector('.edit-button');
 let profileInfoName = document.querySelector('.profile-info__title');
 let profileInfoJob = document.querySelector('.profile-info__subtitle');
-let nameInput = document.querySelector('.popup__item_name');
-let jobInput = document.querySelector('.popup__item_job');
-// let submitButton = document.querySelector('.submit-button');
+let nameInput = document.querySelector('.form__item_el_name');
+let jobInput = document.querySelector('.form__item_el_job');
 
-function openPopup() {
+function openForm() {
   nameInput.value = profileInfoName.textContent;
   jobInput.value = profileInfoJob.textContent;
-  popupElement.classList.add('popup_opened');
+  formElement.classList.add('form_opened');
 }
 
-function closePopup() {
-  popupElement.classList.remove('popup_opened');
+function closeForm() {
+  formElement.classList.remove('form_opened');
 }
 
 //очищаем поле ввода "Имя"
@@ -31,11 +30,11 @@ function handleFormSubmit(evt) {
   evt.preventDefault();
   profileInfoName.textContent = nameInput.value;
   profileInfoJob.textContent = jobInput.value;
-  closePopup(evt);
+  closeForm(evt);
 }
 
-popupOpenButtonElement.addEventListener('click', openPopup); //щелчок по кнопке редактирования профиля
-popupCloseButtonElement.addEventListener('click', closePopup); //щелчок по кнопке закрытия окна редактирования профиля
+formOpenButtonElement.addEventListener('click', openForm); //щелчок по кнопке редактирования профиля
+formCloseButtonElement.addEventListener('click', closeForm); //щелчок по кнопке закрытия окна редактирования профиля
 //nameInput.addEventListener('click', clickNameInput); //щелчок по полю ввода "Имя"
 //jobInput.addEventListener('click', clickJobInput);   //щелчок по полю ввода "Род занятий"
-submitButton.addEventListener('click', handleFormSubmit); //щелчок по кнопке "Сохранить"
+formElement.addEventListener('submit', handleFormSubmit); //щелчок по кнопке "Сохранить"
