bc2be635e8dc38524679cd4e20c6aaea00bea7f6
Anatoly Koshlyak <air721@yandex.ru>
2023-01-18 21:26:22 +0600

    Add like

--- scripts/index.js
+++ scripts/index.js
@@ -5,6 +5,9 @@ let profileInfoName = document.querySelector('.profile-info__title');
 let profileInfoJob = document.querySelector('.profile-info__subtitle');
 let nameInput = document.querySelector('.form__item_el_name');
 let jobInput = document.querySelector('.form__item_el_job');
+//const heartActiv = document.querySelector('.heart');
+
+
 
 function openForm() {
   nameInput.value = profileInfoName.textContent;
@@ -33,8 +36,24 @@ function handleFormSubmit(evt) {
   closeForm(evt);
 }
 
+
+
 formOpenButtonElement.addEventListener('click', openForm); //щелчок по кнопке редактирования профиля
 formCloseButtonElement.addEventListener('click', closeForm); //щелчок по кнопке закрытия окна редактирования профиля
 //nameInput.addEventListener('click', clickNameInput); //щелчок по полю ввода "Имя"
 //jobInput.addEventListener('click', clickJobInput);   //щелчок по полю ввода "Род занятий"
 formElement.addEventListener('submit', handleFormSubmit); //щелчок по кнопке "Сохранить"
+
+const cardElement = document.querySelector('.element');
+//const cardElementContainer = cardElement.querySelector('.element__container').cloneNode(true);
+const cardElementContainer = cardElement.querySelector('.element__container');
+/*cardElementContainer.querySelector('.heart').addEventListener('click', function(evt) {
+  evt.target.classList.toggle('heart_activ');
+});*/
+const buttonHeart = cardElementContainer.querySelector('.heart');
+
+buttonHeart.addEventListener('click', function(evt) {
+  evt.target.classList.toggle('heart_activ');
+  console.log(evt);
+});
+
