25c02af55369c0d4349eca713555aa0172714240
Anatoly Koshlyak <air721@yandex.ru>
2023-01-21 20:36:10 +0600

    "refact popup"

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,11 @@
-let formElement = document.querySelector('.form');
-let formCloseButtonElement = formElement.querySelector('.form__close');
-let formOpenButtonElement = document.querySelector('.edit-button');
+const popupElement = document.querySelector('.popup');
+const formEditOpenButton = document.querySelector('.edit-button');
+const formEditCloseButton = popupElement.querySelector('.form__close_el_edit');
+const formAddOpenButton = document.querySelector('.add-button');
+const formAddCloseButton = popupElement.querySelector('.form__close_el_add');
+const formContainerEdit = popupElement.querySelector('.form__container_el_edit');
+const formContainerAdd = popupElement.querySelector('.form__container_el_add');
+
 let profileInfoName = document.querySelector('.profile-info__title');
 let profileInfoJob = document.querySelector('.profile-info__subtitle');
 let nameInput = document.querySelector('.form__item_el_name');
@@ -9,14 +14,34 @@ let jobInput = document.querySelector('.form__item_el_job');
 
 
 
-function openForm() {
-  nameInput.value = profileInfoName.textContent;
-  jobInput.value = profileInfoJob.textContent;
-  formElement.classList.add('form_opened');
+function openPopup() {
+  popupElement.classList.add('popup_opened');
 }
 
-function closeForm() {
-  formElement.classList.remove('form_opened');
+function closePopup() {
+  popupElement.classList.remove('popup_opened');
+}
+
+function openFormEdit() {
+  nameInput.value = profileInfoName.textContent;
+  jobInput.value = profileInfoJob.textContent;
+  openPopup();
+  formContainerEdit.classList.add('form__container_opened');
+  formContainerAdd.classList.remove('form__container_opened');
+}
+
+function closeFormEdit() {
+  closePopup();
+}
+
+function openFormAdd() {
+  openPopup();
+  formContainerEdit.classList.remove('form__container_opened');
+  formContainerAdd.classList.add('form__container_opened');
+}
+
+function closeFormAdd() {
+  closePopup();
 }
 
 //очищаем поле ввода "Имя"
@@ -29,31 +54,25 @@ function closeForm() {
   jobInput.value = '';
 }*/
 
-function handleFormSubmit(evt) {
+function handleFormEditSubmit(evt) {
   evt.preventDefault();
   profileInfoName.textContent = nameInput.value;
   profileInfoJob.textContent = jobInput.value;
-  closeForm(evt);
+  closeFormEdit(evt);
+}
+
+function handleFormAddSubmit(evt) {
+  evt.preventDefault();
+  closeFormAdd(evt);
 }
 
 
 
-formOpenButtonElement.addEventListener('click', openForm); //щелчок по кнопке редактирования профиля
-formCloseButtonElement.addEventListener('click', closeForm); //щелчок по кнопке закрытия окна редактирования профиля
+formEditOpenButton.addEventListener('click', openFormEdit); //щелчок по кнопке редактирования профиля
+formEditCloseButton.addEventListener('click', closeFormEdit); //щелчок по кнопке закрытия окна редактирования профиля
+formAddOpenButton.addEventListener('click', openFormAdd);
+formAddCloseButton.addEventListener('click', closeFormAdd);
 //nameInput.addEventListener('click', clickNameInput); //щелчок по полю ввода "Имя"
 //jobInput.addEventListener('click', clickJobInput);   //щелчок по полю ввода "Род занятий"
-formElement.addEventListener('submit', handleFormSubmit); //щелчок по кнопке "Сохранить"
-
-const cardElement = document.querySelector('.element');
-//const cardElementContainer = cardElement.querySelector('.element__container').cloneNode(true);
-const cardElementContainer = cardElement.querySelector('.element__container');
-/*cardElementContainer.querySelector('.heart').addEventListener('click', function(evt) {
-  evt.target.classList.toggle('heart_activ');
-});*/
-const buttonHeart = cardElementContainer.querySelector('.heart');
-
-buttonHeart.addEventListener('click', function(evt) {
-  evt.target.classList.toggle('heart_activ');
-  console.log(evt);
-});
-
+formContainerEdit.addEventListener('submit', handleFormEditSubmit); //щелчок по кнопке "Сохранить"
+formContainerAdd.addEventListener('submit', handleFormAddSubmit);
