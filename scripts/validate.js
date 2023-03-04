01640fde262a5f8269a3f962ca68a669993898fb
Anatoly Koshlyak <air721@yandex.ru>
2023-03-04 13:01:04 +0600

    Сделал валидацию полей в форме редактирования профиля

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,28 +1,40 @@
-const popupEdit = document.querySelector(".popup__edit");
-//const nameInputProfile = popupEdit.querySelector(".popup__item_el_name");
-const nameInputProfile = popupEdit.querySelector(".popup__item");
-const nameInputError = popupEdit.querySelector(`.${nameInputProfile.id}-error`);
+const form = document.querySelector(".form");
+const formInput = form.querySelector(".form__item");
+const formError = form.querySelector(`.${formInput.id}-error`);
 
-function showInputError (element) {
-  element.classList.add('popup__item_type_error');
-  nameInputError.textContent = 'Вы пропустили это поле';
-  //nameInputError.classList.add('popup__item_el_name-error_activ');
-  nameInputError.classList.add('popup__item-error_activ');
+function showInputError (formElement, inputElement, errorMessage) {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add('form__item_type_error');
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add('form__item-error_activ');
 }
 
-function hideInputError (element) {
-  element.classList.remove('popup__item_type_error');
-  nameInputError.textContent = '';
-  //nameInputError.classList.add('popup__item_el_name-error_activ');
-  nameInputError.classList.remove('popup__item-error_activ');
+function hideInputError (formElement, inputElement) {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove('form__item_type_error');
+  errorElement.classList.remove('form__item-error_activ');
+  errorElement.textContent = '';
 }
 
-function isValid () {
-  if (!nameInputProfile.validity.valid) {
-    showInputError (nameInputProfile);
+function checkInputValidity (formElement, inputElement) {
+  if (!inputElement.validity.valid) {
+    showInputError (formElement, inputElement, inputElement.validationMessage);
   }else {
-    hideInputError (nameInputProfile)
+    hideInputError (formElement, inputElement);
   }
 }
 
-nameInputProfile.addEventListener ('input', isValid);
+function setEventListeners (formElement) {
+  const inputList = Array.from(formElement.querySelectorAll('.form__item'));
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', function() {
+      checkInputValidity(formElement, inputElement);
+      });
+    });
+}
+
+setEventListeners (form);
+
+formInput.addEventListener ('input', function () {
+  checkInputValidity(form, formInput);
+});
